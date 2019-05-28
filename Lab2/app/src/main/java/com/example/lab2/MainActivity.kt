3095a355c376226ba27d8ae8cdd8b3a62bc4f139
Bandarban <Bandarban@users.noreply.github.com>
2019-05-28 16:50:25 +0500

    Lab 2 is done

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.lab2
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
     private var imageView: ImageView? = null
+    private var image: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         imageView = findViewById(R.id.imageView)
+        imageView!!.setImageBitmap(image)
         startImageDownloadThread()
+
     }
 
 
@@ -26,15 +30,17 @@ class MainActivity : AppCompatActivity() {
                 //Do Network Request
                 val url = URL("https://thispersondoesnotexist.com/image")
                 val bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                image = bmp
                 runOnUiThread {
                     //Update UI
-                    imageView!!.setImageBitmap(bmp)
+                    imageView!!.setImageBitmap(image)
                 }
             }.start()
         } catch (e: Exception) { // Catch the download exception
             e.printStackTrace()
         }
     }
+
 
     fun like(view: View) {
         startImageDownloadThread()
